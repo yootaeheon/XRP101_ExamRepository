8b808bcc7315b2c73c0f206e7184abf02e909be5
TaeHeon Ryoo <taroni55@naver.com>
2024-11-19 11:26:36 +0900

    [Fix] Q_01번 문제 제출

--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -7,16 +7,16 @@ public class CubeManager : MonoBehaviour
     [SerializeField] private GameObject _cubePrefab;
 
     private CubeController _cubeController;
-    private Vector3 _cubeSetPoint;
+    public Vector3 _cubeSetPoint;
 
-    private void Awake()
-    {
-        SetCubePosition(3, 0, 3);
-    }
-
+  
     private void Start()
     {
+        // 함수의 순서를 조정하여 큐브를 생성하고 큐브의 좌표를 수정하게끔 순서를 조정
+        // 이후 위치를 변경해줌 
         CreateCube();
+        SetCubePosition(3, 0, 3);
+        _cubeController.SetPosition();
     }
 
     private void SetCubePosition(float x, float y, float z)
@@ -24,7 +24,11 @@ public class CubeManager : MonoBehaviour
         _cubeSetPoint.x = x;
         _cubeSetPoint.y = y;
         _cubeSetPoint.z = z;
-        _cubeController.SetPosition();
+
+        // SetCubePosition() 함수는 _cubeSetPoint의 좌표를 변경해줌
+        // 하지만 정작 움직이는 함수인 _cubeController.SetPosition() 함수는 _cubeController.SetPoint 좌표로 설정되어 있음
+        // _cubeSetPoint의 좌표를 _cubeController.SetPoint 좌표로 넘겨주어 해결 함
+        _cubeController.SetPoint = _cubeSetPoint;
     }
 
     private void CreateCube()
